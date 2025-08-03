ab8f26add06a64799a3b955ff81ed732507f16f4
ayushchaudhary21 <chaudharyayush269@gmail.com>
2025-08-03 21:23:48 +0530

    complete the project

--- src/main/java/com/note/Notes/Controller/NotesController.java
+++ src/main/java/com/note/Notes/Controller/NotesController.java
@@ -4,14 +4,20 @@ import com.note.Notes.Entity.Notes;
 import com.note.Notes.Service.NoteServiceInterface;
 import com.note.Notes.Service.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RestController
-@RequestMapping("Notes")
+@RequestMapping("/Notes")
 public class NotesController {
     @Autowired
     private NoteServiceInterface noteServiceInterface;
@@ -20,46 +26,53 @@ public class NotesController {
 
 
 
-    @PostMapping("/{userName}")
-    public ResponseEntity<?> createNotes(@PathVariable("userName") String userName,
-                                @RequestBody Notes notes)
+    @PostMapping()
+    public ResponseEntity<?> createNotes(@RequestBody Notes notes)
     {
-
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        String userName=authentication.getName();
         return new ResponseEntity<>(noteServiceInterface.saveNotes(userName,notes),HttpStatus.CREATED);
 
     }
 
 
 
-    @GetMapping("/getbyid/{id}/{userName}")
-    public ResponseEntity<?> getById(@PathVariable String userName,@PathVariable ("id") long id)
+    @GetMapping("/getbyid/{id}")
+    public ResponseEntity<?> getById(@PathVariable ("id") long id)
     {
+        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+        String userName=authentication.getName();
         return new ResponseEntity<>(noteServiceInterface.getNotesById(userName,id),HttpStatus.OK);
     }
 
 
-    @GetMapping("/getbytitle/{title}/{userName}")
-    public ResponseEntity<?> getByUserName(@PathVariable ("userName") String userName
-                                           , @PathVariable("title") String title)
+    @GetMapping("/getbytitle/{title}")
+    public ResponseEntity<?> getByUserName( @PathVariable("title") String title)
     {
+        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+        String userName=authentication.getName();
          return new ResponseEntity<>(noteServiceInterface.getbyName(userName,title),HttpStatus.OK);
     }
 
 
 
-      @GetMapping("getAll/{userName}")
-        public ResponseEntity<?> getALl(@PathVariable("userName") String userName)
+      @GetMapping("getAll")
+        public ResponseEntity<?> getALl()
         {
+            Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+            String userName=authentication.getName();
             return new ResponseEntity<>(noteServiceInterface.getAll(userName),HttpStatus.OK);
         }
 
 
 
 
-    @DeleteMapping("/deletebytitle/{title}/{userName}")
+    @DeleteMapping("/deletebytitle/{title}")
     public ResponseEntity<?> deteleByTitle(@PathVariable ("title") String title
-                                              ,@PathVariable("userName") String userName)
+                                              )
         {
+            Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+            String userName=authentication.getName();
             noteServiceInterface.deleteByTitle(title,userName);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
@@ -67,10 +80,12 @@ public class NotesController {
 
 
 
-        @PutMapping("/updatebytitle/{userName}/{title}")
-     public ResponseEntity<?> updateByTitle(@PathVariable("userName") String userName,@PathVariable ("title") String title,
+        @PutMapping("/updatebytitle/{title}")
+     public ResponseEntity<?> updateByTitle(@PathVariable ("title") String title,
                                             @RequestBody Notes note)
         {
+            Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+            String userName=authentication.getName();
             return new ResponseEntity<>(noteServiceInterface.updateByTitle(userName,title,note),HttpStatus.OK);
         }
 }
